4c12302a23926fa9de97f1b9dcc9da4cf45c6d96
sspatari <spatari.stanislav@gmail.com>
2017-12-21 04:58:10 +0100

    Parallelis Jacobi algorithm in Lab 2 Exercise 3

---
+++ Lab2/Parallel_Processed_Code/Exercise3/solver-jacobi.cpp
@@ -0,0 +1,86 @@
+/*************************************************************************
+** Iterative solver: Jacobi method
+**
+** Author:   RW
+**
+*************************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+
+/*
+** The iterative computation terminates, if each element has changed
+** by at most 'eps', as compared to the last iteration.
+*/
+extern double eps;
+
+
+/* Auxiliary Functions *************************************************** */
+
+extern double ** New_Matrix(int m, int n);
+extern void Delete_Matrix(double **matrix);
+
+
+/* Jacobi iteration ***************************************************** */
+
+/*
+** Execute Jacobi iteration on the n*n matrix 'a'.
+*/
+int solver(double **a, int n)
+{
+	int i,j;
+	double h;
+	double diff;    /* Maximum change since the last iteration */
+	int k = 0;      /* Counts iterations (for statistics only ...) */
+	double **b = New_Matrix(n,n);  /* Auxiliary matrix for result */
+
+	if (b == NULL) {
+		cerr << "Jacobi: Can't allocate matrix\n";
+		exit(1);
+	}
+
+	/*
+	** Iterate until convergence is achieved. Here: until the maximum
+	** change of a matrix element is smaller or equal than 'eps'.
+	*/
+	do {
+		diff = 0;
+
+		#pragma omp parallel for collapse(2) private(h) reduction(max : diff)
+		// By default, loop iteration counters in OpenMP loop constructs (in this case the i variable) in the for loop are set to private variables.
+		// observation collapse makes j by default private, if we remove collapse then we need to set j as private.
+		for (i=1; i<n-1; i++) {
+			for (j=1; j<n-1; j++) {
+				b[i][j] = 0.25 * (a[i][j-1] + a[i-1][j]
+								  + a[i+1][j] + a[i][j+1]);
+
+				/* Determine the maximum change of the matrix elements */
+				h = fabs(a[i][j] - b[i][j]);
+				if (h > diff)
+					diff = h;
+			}
+		}
+
+		/*
+		** Copy intermediate result into matrix 'a'
+		*/
+		#pragma omp parallel for collapse(2)
+		// By default, loop iteration counters in OpenMP loop constructs (in this case the i variable) in the for loop are set to private variables.
+		// observation collapse makes the j also private by default, if we remove collapse j should be set private.
+		for (i=1; i<n-1; i++) {
+			for (j=1; j<n-1; j++) {
+				a[i][j] = b[i][j];
+			}
+		}
+
+		k++;
+	} while (diff > eps);
+
+	Delete_Matrix(b);
+
+	return k;
+}
